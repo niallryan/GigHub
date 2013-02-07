abc329ebfc239325afb4f9c2725464d68842a619
Niall Ryan <ryann4@tcd.ie>
2013-02-07 12:21:03 +0000

    Created GigHub Rails app and added Bootstrap files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GigHub::Application.config.secret_token = '87da1a3617127a12182687471f6aa46d4c32bf2e08cb474965b2b0c383fa33a39c8366f99e489c87c0f07ccf5f8c740df1389d2f3e3635c0dda55d794b9bb52f'
