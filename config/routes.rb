0aa492297124a96fb5d4ece85c67ed287362e391
Niall Ryan <ryann4@tcd.ie>
2013-03-26 14:28:51 +0000

    Updated Profile Page - scoped status updates.

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ GigHub::Application.routes.draw do
   resources :statuses
   get 'feed', to: 'statuses#index', :as => :feed
 
-
+  get '/:id', to: 'profiles#show'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
