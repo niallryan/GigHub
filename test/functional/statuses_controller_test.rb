9aeb561d0c7fc4fdcdec172dc7529e1509644e06
Niall Ryan <ryann4@tcd.ie>
2013-04-08 20:03:30 +0100

    Wrote tests for creating and updating statuses.

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -38,9 +38,25 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to status_path(assigns(:status))
   end
 
+  test "should create status for current user when logged in" do
+    sign_in users(:niall)
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:emma).id }
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:niall).id
+  end
+
   test "should show status" do
     get :show, id: @status
     assert_response :success
+  end
+
+  test "should redirect edit when not logged in" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
   end
 
   test "should get edit when logged in" do
@@ -59,6 +75,20 @@ class StatusesControllerTest < ActionController::TestCase
     sign_in users(:niall)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should update status for current user when logged in" do
+    sign_in users(:niall)
+    put :update, id: @status, status: { content: @status.content, user_id: users(:niall).id }
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:niall).id
+  end
+
+  test "should not update status if nothing has changed" do
+    sign_in users(:niall)
+    put :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:niall).id
   end
 
   test "should destroy status" do
